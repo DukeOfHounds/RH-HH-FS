d44c4abb3576bfc122e3d536d608f3a098b9941f
Connor Hartzog <hartzogcr@gmail.com>
2025-01-25 12:34:29 -0500

    Hadeness

--- Assets/Scripts/ThermalModulator.cs
+++ Assets/Scripts/ThermalModulator.cs
@@ -2,16 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class ThermalModulator : MonoBehaviour
 {
     
+    private XRBaseInteractable xRBaseInteractable;
 
     public RestrictedThermalRange OjectThermalFloat; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        xRBaseInteractable = GetComponent<XRBaseInteractable>();
+        xRBaseInteractable.hoverEntered.AddListener(OnHoverEnter);
     }
 
     // Update is called once per frame
@@ -19,9 +24,20 @@ public class ThermalModulator : MonoBehaviour
     {
         
     }
+    
+    public void OnHoverEnter(HoverEnterEventArgs args){
 
-    public void RadiateThermalEnergy(){
-        Debug.Log("Interacting with RadiateThermalEnergy");
+
+        var interactor = args.interactorObject;
+
+        if (interactor is NearFarInteractor nearFar){
+        
+         Debug.Log("Interacting with RadiateThermalEnergy with nearFar handedness: " + nearFar.handedness);
+
+        }
+
     }
+
+
 
 }
